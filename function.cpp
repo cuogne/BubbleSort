b4fd1565f8760baf9e6455a327b975068e96483e
Cường <143596996+cuogne@users.noreply.github.com>
2023-09-19 00:58:00 +0700

    FunctionBS.cpp

--- function.cpp
+++ function.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 using namespace std ;
 
-void BubbleSort ( int a[] , int n ) {
-  int i, j ;
-  for ( int i = 0 ; i < n - 1 ; i++ ){
-    for ( int j = 1 ; j < n - i - 1 ; j++ ){
-      if(a[j] > a[j+1]){
-        int temp = a[j] ;
-        a[j] = a[j+1] ;
-        a[j+1] = temp ; 
-      }
+void BubbleSort ( int a[] , int n){ // thuật toán BubbleSort 
+    for ( int i = 0 ; i < n ; i++ ){
+        for ( int j = i + 1 ; j < n ; j++ ){
+            if ( a[i] > a[j] ){
+                int temp = a[j];
+                a[j] = a[i];
+                a[i] = temp;
+            }
+        }
     }
-  }
 }
-      
+
+int main () {
+    int n ; cin >> n; // khai báo biến và nhập só lượng mảng 
+    int a[n];
+    for ( int i = 0 ; i < n ; i++){
+        cin >> a[i] ;
+    }
+    BubbleSort(a,n); // truyền tham số cho hàm BubbleSort() 
+    for ( int i = 0 ; i < n ; i++){
+        cout << a[i] <<" "; 
+    }
+}
